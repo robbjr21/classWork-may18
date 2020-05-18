cf0ea7691c5d6c184420ded8d943e31290c203cb
Rob Baldoni <robert.baldoni@duke.edu>
2020-05-18 11:31:03 -0400

    Module format for functions

--- calculator.py
+++ calculator.py
@@ -1,28 +1,46 @@
 # calculator.py
-a = input("Enter the first number: ")
-b = input("Enter the second number: ")
-a = int(a)
-b = int(b)
-print("The numbers you entered are {} and {} " .format(a, b))
 
-c = input("Enter a command: ")
+def calc_input():
+    a = input("Enter the first number: ")
+    b = input("Enter the second number: ")
+    a = int(a)
+    b = int(b)
+    print("The numbers you entered are {} and {} " .format(a, b))
+    return a, b
 
-if c == "a":
+def add(a, b):
     print("Add")
     answer = a + b
     print("{} + {} = {}".format(a, b, answer))
-elif c == "s":
+
+def subtract(a, b):
     print("Subtract")
     answer = a - b
-    print("{} + {} = {}".format(a, b, answer))
-elif c == "m":
+    print("{} - {} = {}".format(a, b, answer))
+
+def multiply(a, b):
     print("Multiply")
     answer = a*b
-    print("{} + {} = {}".format(a, b, answer))
-elif c == "d":
+    print("{} x {} = {}".format(a, b, answer))
+
+def divide(a, b):
     print("Divide")
     answer = a/b
-    print("{} + {} = {}".format(a, b, answer))
-else:
-    print("{} is not a valid command".format(c))
+    print("{} / {} = {}".format(a, b, answer))
+
+def math_command(a, b):
+    c = input("Enter a command: ")
+    if c == "a":
+        add(a, b)
+    elif c == "s":
+        subtract(a, b)
+    elif c == "m":
+        multiply(a, b)
+    elif c == "d":
+        divide(a, b)
+    else:
+        print("{} is not a valid command".format(c))
+
+x, y = calc_input()
+math_command(x,y)
 print("Finished")
